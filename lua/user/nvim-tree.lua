8ae1a23fc88f93e7a5d1d4f9d3a37400fae9da32
philor1 <scheibe.philipp@gmail.com>
2022-05-10 23:13:45 +0200

    nvim-tree update-fix

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,23 +1,44 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_root_folder_modifier = ":t"
+vim.g.nvim_tree_add_trailing = 0
+vim.g.nvim_tree_group_empty = 0
+vim.g.nvim_tree_icon_padding = ' '
+vim.g.nvim_tree_symlink_arrow = ' >> '
+vim.g.nvim_tree_respect_buf_cwd = 1
+vim.g.nvim_tree_create_in_closed_folder = 1
+vim.g.nvim_tree_special_files = {
+	Makefile = 1,
+	MAKEFILE = 1,
+	README = 1,
+}
+vim.g.nvim_tree_show_icons = {
+	git = 1,
+	folders = 1,
+	files = 1,
+	folder_arrows = 1,
+}
 vim.g.nvim_tree_icons = {
-	default = "",
+	default = "",
 	symlink = "",
 	git = {
-		unstaged = "",
-		staged = "S",
+		unstaged = "✗",
+		staged = "✓",
 		unmerged = "",
 		renamed = "➜",
+		untracked = "★",
 		deleted = "",
-		untracked = "U",
 		ignored = "◌",
 	},
 	folder = {
+		arrow_open = "",
+		arrow_closed = "",
 		default = "",
 		open = "",
 		empty = "",
 		empty_open = "",
 		symlink = "",
+		symlink_open = "",
 	},
 }
 
@@ -34,51 +55,30 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
+	auto_reload_on_write = true,
 	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = {},
-	auto_close = false,
-	open_on_tab = false,
 	hijack_cursor = true,
+	hijack_netrw = true,
+	hijack_unnamed_buffer_when_opening = false,
+	ignore_buffer_on_setup = false,
+	open_on_setup = false,
+	open_on_setup_file = false,
+	open_on_tab = false,
+	sort_by = "name",
 	update_cwd = true,
 	update_to_buf_dir = {
 		enable = true,
 		auto_open = true,
-	},
-	diagnostics = {
-		enable = true,
-		icons = {
-			hint = "",
-			info = "",
-			warning = "",
-			error = "",
-		},
-	},
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-		ignore_list = {},
-	},
-	system_open = {
-		cmd = nil,
-		args = {},
-	},
-	filters = {
-		dotfiles = false,
-		custom = {},
-	},
-	git = {
-		enable = true,
-		ignore = true,
-		timeout = 500,
 	},
 	view = {
 		width = 22,
 		height = 30,
 		hide_root_folder = false,
 		side = "left",
-		auto_resize = true,
+		preserve_window_proportions = false,
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
 		mappings = {
 			custom_only = false,
 			list = {
@@ -87,25 +87,89 @@ nvim_tree.setup({
 				{ key = "v", cb = tree_cb("vsplit") },
 			},
 		},
-		number = false,
-		relativenumber = false,
+	},
+	renderer = {
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+			git_placement = "before",
+		},
+	},
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	ignore_ft_on_setup = {},
+	system_open = {
+		cmd = "",
+		args = {},
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	filters = {
+		dotfiles = false,
+		custom = {},
+		exclude = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 400,
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = true,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
 	},
 	trash = {
 		cmd = "trash",
 		require_confirm = true,
 	},
-	quit_on_open = 0,
-	git_hl = 1,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
-	show_icons = {
-		git = 1,
-		folders = 1,
-		files = 1,
-		folder_arrows = 1,
-		tree_width = 30,
+	log = {
+		enable = false,
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
+		},
 	},
 })
-
--- remove signcolumn from nvim_tree
---require('nvim-tree.view').View.winopts.signcolumn = 'no'

45015a873b264626e93b1f459adc4712ee4db4f8
philor1 <scheibe.philipp@gmail.com>
2022-08-19 09:15:09 +0200

    packer-settings for custom commits in plugins (no merge errors)

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -36,6 +36,25 @@ packer.init({
 			return require("packer.util").float({ border = "rounded" })
 		end,
 	},
+	git = {
+		cmd = "git", -- The base command for git operations
+		subcommands = { -- Format strings for git subcommands
+			update = "pull --progress --rebase=true",
+			install = "clone --depth %i --no-single-branch --progress",
+			fetch = "fetch --depth 999999 --progress",
+			checkout = "checkout %s --",
+			update_branch = "merge @{u}",
+			current_branch = "branch --show-current",
+			diff = "log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD",
+			diff_fmt = "%%h %%s (%%cr)",
+			get_rev = "rev-parse --short HEAD",
+			get_msg = "log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1",
+			submodules = "submodule update --init --recursive --progress",
+		},
+		depth = 1, -- Git clone depth
+		clone_timeout = 60, -- Timeout, in seconds, for git clones
+		default_url_format = "https://github.com/%s", -- Lua format string used for "aaa/bbb" style plugins
+	},
 })
 
 -- Install your plugins here

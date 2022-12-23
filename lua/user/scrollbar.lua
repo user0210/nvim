12ef4e3c74fdf7b63f73f006ad69259398bec37b
philipp <scheibe.philipp@gmail.com>
2022-12-23 21:27:34 +0100

    update

--- lua/user/scrollbar.lua
+++ lua/user/scrollbar.lua
@@ -9,79 +9,80 @@ require("scrollbar.handlers.search").setup({
 
 local colors = require("colorscheme").colors
 
+vim.api.nvim_set_hl(0, "ScrollbarHandle", 			{ bg = colors.base01a,	fg = colors.base00a })
+vim.api.nvim_set_hl(0, "ScrollbarCursor", 			{ bg = colors.base01a,	fg = colors.base00a })
+vim.api.nvim_set_hl(0, "ScrollbarCursorHandle", 	{ bg = colors.base01a,	fg = colors.base00a })
+vim.api.nvim_set_hl(0, "ScrollbarError", 			{ bg = colors.base00,	fg = colors.base08a })
+vim.api.nvim_set_hl(0, "ScrollbarErrorHandle", 		{ bg = colors.base01a,	fg = colors.base08 })
+vim.api.nvim_set_hl(0, "ScrollbarWarn", 			{ bg = colors.base00,	fg = colors.base09a })
+vim.api.nvim_set_hl(0, "ScrollbarWarnHandle", 		{ bg = colors.base01a,	fg = colors.base09 })
+vim.api.nvim_set_hl(0, "ScrollbarSearch", 			{ bg = colors.base00,	fg = colors.base0Aa })
+vim.api.nvim_set_hl(0, "ScrollbarSearchHandle", 	{ bg = colors.base01a,	fg = colors.base0A })
+vim.api.nvim_set_hl(0, "ScrollbarInfo", 			{ bg = colors.base00,	fg = colors.base0Ca })
+vim.api.nvim_set_hl(0, "ScrollbarInfoHandle", 		{ bg = colors.base01a,	fg = colors.base0C })
+vim.api.nvim_set_hl(0, "ScrollbarHint", 			{ bg = colors.base00,	fg = colors.base0Fa })
+vim.api.nvim_set_hl(0, "ScrollbarHintHandle", 		{ bg = colors.base01a,	fg = colors.base0F })
+vim.api.nvim_set_hl(0, "ScrollbarMisc", 			{ bg = colors.base00,	fg = colors.base03a })
+vim.api.nvim_set_hl(0, "ScrollbarMiscHandle", 		{ bg = colors.base01a,	fg = colors.base03 })
+vim.api.nvim_set_hl(0, "ScrollbarGitAdd", 			{ bg = colors.base00,	fg = colors.base0Ba })
+vim.api.nvim_set_hl(0, "ScrollbarGitAddHandle", 	{ bg = colors.base01a,	fg = colors.base0B })
+vim.api.nvim_set_hl(0, "ScrollbarGitChange", 		{ bg = colors.base00,	fg = colors.base0Da })
+vim.api.nvim_set_hl(0, "ScrollbarGitChangeHandle", 	{ bg = colors.base01a,	fg = colors.base0D })
+vim.api.nvim_set_hl(0, "ScrollbarGitDelete", 		{ bg = colors.base00,	fg = colors.base0Fa })
+vim.api.nvim_set_hl(0, "ScrollbarGitDeleteHandle", 	{ bg = colors.base01a,	fg = colors.base0F })
+
+
 scrollbar.setup({
 	show = true,
 	show_in_active_only = false,
-	set_highlights = true,
+	set_highlights = false,
 	folds = 1000, -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
 	max_lines = false, -- disables if no. of lines in buffer exceeds this
-	handle = {
-		text = " ",
-		color = nil,
-		cterm = nil,
-		hide_if_all_visible = false, -- Hides handle if all lines are visible
-	},
-	marks = {
-		Cursor = {
-			text = "•",
-			priority = 0,
-			color = colors.base00a,
-			cterm = nil,
-		},
-		Search = {
-			text = { "-", "=" },
-			priority = 1,
-			color = colors.base0Aa,
-			cterm = nil,
-		},
-		Error = {
-			text = { "-", "=" },
-			priority = 2,
-			color = nil,
-			cterm = nil,
-		},
-		Warn = {
-			text = { "-", "=" },
-			priority = 3,
-			color = nil,
-			cterm = nil,
-		},
-		Info = {
-			text = { "-", "=" },
-			priority = 4,
-			color = nil,
-			cterm = nil,
-		},
-		Hint = {
-			text = { "-", "=" },
-			priority = 5,
-			color = nil,
-			cterm = nil,
-		},
-		Misc = {
-			text = { "-", "=" },
-			priority = 6,
-			color = nil,
-			cterm = nil,
-		},
-		GitAdd = {
-			text = "┆",
-			priority = 7,
-			color = nil,
-			cterm = nil,
-		},
-		GitChange = {
-			text = "┆",
-			priority = 7,
-			color = nil,
-			cterm = nil,
-		},
-		GitDelete = {
-			text = "▁",
-			priority = 7,
-			color = nil,
-			cterm = nil,
-		},
+    handle = {
+        text = " ",
+        hide_if_all_visible = true, -- Hides handle if all lines are visible
+    },
+    marks = {
+        Cursor = {
+            text = "•",
+            priority = 0,
+        },
+        Search = {
+            text = { "-", "=" },
+            priority = 1,
+        },
+        Error = {
+            text = { "-", "=" },
+            priority = 2,
+        },
+        Warn = {
+            text = { "-", "=" },
+            priority = 3,
+        },
+        Info = {
+            text = { "-", "=" },
+            priority = 4,
+        },
+        Hint = {
+            text = { "-", "=" },
+            priority = 5,
+        },
+        Misc = {
+            text = { "-", "=" },
+            priority = 6,
+        },
+        GitAdd = {
+            text = "┆",
+            priority = 7,
+        },
+        GitChange = {
+            text = "┆",
+            priority = 7,
+        },
+        GitDelete = {
+            text = "▁",
+            priority = 7,
+        },
 	},
 	excluded_buftypes = {
 		"terminal",

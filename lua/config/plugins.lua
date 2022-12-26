501de222a2cec9ef49227da393937485c911b6c4
philipp <scheibe.philipp@gmail.com>
2022-12-26 23:48:28 +0100

    update

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,103 +1,91 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.runtimepath:prepend(lazypath)
 require("lazy").setup({
 
-	{"windwp/nvim-autopairs", config = function() require"plugins.autopairs" end, },
-	{"nvim-lualine/lualine.nvim", config = function() require"plugins.lualine" end, },
-	{"akinsho/toggleterm.nvim", config = function() require"plugins.toggleterm" end, },
-	{"ahmedkhalf/project.nvim", config = function() require"plugins.project" end, },
-	{"lukas-reineke/indent-blankline.nvim", config = function() require"plugins.indentline" end, },
-	{"folke/which-key.nvim", config = function() require"plugins.whichkey" end, },
-	{"uga-rosa/ccc.nvim", config = function() require"plugins.ccc" end, },
-	{"karb94/neoscroll.nvim", config = function() require"plugins.neoscroll" end, },
-	{"aserowy/tmux.nvim", config = function() require"plugins.tmux" end, },
-	{"RRethy/nvim-base16", config = function() require"plugins.base16" end, },
-	{"j-hui/fidget.nvim", config = function() require"plugins.fidget" end, },
-	{"mbbill/undotree", config = function() require"plugins.undotree" end, },
-	{"SmiteshP/nvim-navic", config = function() require"plugins.navic" end, },
-	{"RRethy/vim-illuminate", config = function() require"plugins.illuminate" end, },
-	{"wfxr/minimap.vim", config = function() require"plugins.minimap" end, },
-	{"sindrets/diffview.nvim", }, --no need for config
+	{ "windwp/nvim-autopairs", config = function() require"plugins.autopairs" end, },
+	{ "nvim-lualine/lualine.nvim", config = function() require"plugins.lualine" end, },
+	{ "akinsho/toggleterm.nvim", config = function() require"plugins.toggleterm" end, },
+	{ "ahmedkhalf/project.nvim", config = function() require"plugins.project" end, },
+	{ "lukas-reineke/indent-blankline.nvim", config = function() require"plugins.indentline" end, },
+	{ "folke/which-key.nvim", config = function() require"plugins.whichkey" end, },
+	{ "uga-rosa/ccc.nvim", config = function() require"plugins.ccc" end, },
+	{ "karb94/neoscroll.nvim", config = function() require"plugins.neoscroll" end, },
+	{ "aserowy/tmux.nvim", config = function() require"plugins.tmux" end, },
+	{ "RRethy/nvim-base16", config = function() require"plugins.base16" end, },
+	{ "j-hui/fidget.nvim", config = function() require"plugins.fidget" end, },
+	{ "mbbill/undotree", config = function() require"plugins.undotree" end, },
+	{ "SmiteshP/nvim-navic", config = function() require"plugins.navic" end, },
+	{ "RRethy/vim-illuminate", config = function() require"plugins.illuminate" end, },
+	{ "wfxr/minimap.vim", config = function() require"plugins.minimap" end, },
+	{ "sindrets/diffview.nvim", }, --no need for config
 
-	{"lewis6991/gitsigns.nvim", config = function() require"plugins.gitsigns" end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
+	{ "lewis6991/gitsigns.nvim", config = function() require"plugins.gitsigns" end,
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-	{"nvim-telescope/telescope.nvim", config = function() require"plugins.telescope" end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
+	{ "nvim-telescope/telescope.nvim", config = function() require"plugins.telescope" end,
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-	{"nvim-tree/nvim-tree.lua", config = function() require"plugins.nvim-tree" end,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
+	{ "nvim-tree/nvim-tree.lua", config = function() require"plugins.nvim-tree" end,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
-	{"akinsho/bufferline.nvim", config = function() require"plugins.bufferline" end,
-		dependencies = {
-			"famiu/bufdelete.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
+	{ "akinsho/bufferline.nvim", config = function() require"plugins.bufferline" end,
+		dependencies = { "famiu/bufdelete.nvim", "nvim-tree/nvim-web-devicons" },
 	},
-	{"petertriho/nvim-scrollbar", config = function() require"plugins.scrollbar" end,
-		dependencies = {
-			"kevinhwang91/nvim-hlslens",
-		},
+	{ "petertriho/nvim-scrollbar", config = function() require"plugins.scrollbar" end,
+		dependencies = { "kevinhwang91/nvim-hlslens" },
 	},
-	{"VonHeikemen/lsp-zero.nvim", config = function() require"plugins.lsp" end,
-	  dependencies = {
-		-- LSP Support
-		"neovim/nvim-lspconfig",
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-
-		-- Autocompletion
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"saadparwaiz1/cmp_luasnip",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-		"onsails/lspkind.nvim",
-
-		-- Snippets
-		"L3MON4D3/LuaSnip",
-		"rafamadriz/friendly-snippets",
-
-		-- Null-LS
-		"jose-elias-alvarez/null-ls.nvim",
-		"jayp0521/mason-null-ls.nvim",
-
-		-- misc
-		"Maan2003/lsp_lines.nvim",
-	  },
+	{ "numToStr/Comment.nvim", config = function() require"plugins.comment" end,
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 	},
-	{"nvim-treesitter/nvim-treesitter", config = function() require"plugins.treesitter" end,
-		dependencies = {
-			{"nvim-treesitter/nvim-treesitter-context", config = function() require"plugins.context" end, },
-			"JoosepAlviste/nvim-ts-context-commentstring",
+	{ "mfussenegger/nvim-dap", config = function() require"plugins.dap" end,
+		dependencies = { "rcarriga/nvim-dap-ui" },
+	},
+	{ "nvim-treesitter/nvim-treesitter", config = function() require"plugins.treesitter" end,
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring",
+			{ "nvim-treesitter/nvim-treesitter-context", config = function() require"plugins.context" end, },
 		},
 		build = ":TSUpdate",
 	},
-	{"numToStr/Comment.nvim", config = function() require"plugins.comment" end,
+	{ "VonHeikemen/lsp-zero.nvim", config = function() require"plugins.lsp" end,
 		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
+			-- LSP Support
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			-- Autocompletion
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
+			"onsails/lspkind.nvim",
+			-- Snippets
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			-- Null-LS
+			"jose-elias-alvarez/null-ls.nvim",
+			"jayp0521/mason-null-ls.nvim",
+			-- misc
+			"Maan2003/lsp_lines.nvim",
 		},
 	},
-	{"mfussenegger/nvim-dap", config = function() require"plugins.dap" end,
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-		},
+},
+{
+	defaults = {
+		lazy = false, -- should plugins be lazy-loaded?
+	},
+	ui = {
+		border = "rounded",
 	},
 })

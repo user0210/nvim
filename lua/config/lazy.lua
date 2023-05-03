34349f0fd29769dabd15eeac4c0ecad83c20d795
philipp <scheibe.philipp@gmail.com>
2023-05-03 09:32:11 +0200

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.dap.nlua" },
+		{ import = "lazyvim.plugins.extras.util.project" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/override with your plugins
 		{ import = "plugins" },
